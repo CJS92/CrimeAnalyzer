6a7aba85caf1dac571d7a887a21961ac5bf21412
Cody Sloan <46944830+CJS92@users.noreply.github.com>
2019-04-07 19:43:52 -0500

    Add files via upload

---
+++ CrimeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CrimeAnalyzer
+{
+
+    internal class CrimeData
+    {
+        // this contains the list of crimes committed
+        public int Year { get; set; }
+        public int Population { get; set; }
+        public int ViolentCrime { get; set; }
+        public int Murder { get; set; }
+        public int Rape { get; set; }
+        public int Robbery { get; set; }
+        public int AggravatedAssault { get; set; }
+        public int PropertyCrime { get; set; }
+        public int Burglary { get; set; }
+        public int Theft { get; set; }
+        public int MotorVehicleTheft { get; set; }
+        public CrimeData()
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Program for IT 2040
+// Cody Sloan
+// 04/05/2019
+
+namespace CrimeAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Crimes crimes = new Crimes(args);
+        }
+    }
+}
